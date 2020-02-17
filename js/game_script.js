a5e8abe85a5dba7c1050d912bd90b109d8b1609c
fengwei77 <fengwei77@gmail.com>
2020-02-17 18:10:09 +0800

    接下來是出怪順序 勁量不要重跌 或是間距拉開

--- js/game_script.js
+++ js/game_script.js
@@ -1,21 +1,22 @@
 const show_debug = true;
 const dev_grid_line = 1;
 
-gsap.registerPlugin(gsap, MotionPathPlugin, EaselPlugin, PixiPlugin, TextPlugin, TweenMax, TimelineMax, Power2);
+gsap.registerPlugin(gsap, MotionPathPlugin, EaselPlugin, PixiPlugin, TextPlugin, TweenMax, TimelineMax, Power2, Power0);
 let WIDTH = 1024;
 let HEIGHT = WIDTH * (10 / 16);
 let extend_height = 150;
-let germs_speed = 0.8;
-let germs_speed_base = 1.5;
+let germs_speed = 0.3;
+let germs_speed_base = 4;
 let countdown = 30;
-let damage_ratio = 0.12;
+let damage_ratio = 0.212;
 let health_width = 300;
 let health_width_in = 300;
-let numberOfgerms_pop = 2;
+let level = 3;
+let numberOfgerms_pop = 4;
 let germs_origin_height = 1;
 let germs_origin_fade_in = 0.2;
 let germs_origin_fade_out = 1.2;
-let bg_sprite, state, health, bg_container, germs_pop, all_obj_container;  //基礎設定
+let gameScene, bg_sprite, state, health, bg_container, germs_pop, all_obj_container, germs_no, germs_alive;  //基礎設定
 const germs_fade_out_set = 0.2;
 show_console('WIDTH =' + WIDTH);
 show_console('HEIGHT =' + HEIGHT);
@@ -65,7 +66,7 @@ timer.on('end', elapsed => {
   player_action = false;
   for (let i = 0; i < germs_pop.length; i++) {
     if (germs_pop[i].alpha > 0) {
-      germs_pop[i].alpha  =  .5
+      germs_pop[i].alpha = .5
 
       all_obj_container.destroy();
     }
@@ -148,7 +149,7 @@ let keys = [
 // 遮擋區塊 -z50
 const block_wall = new PIXI.Graphics();
 block_wall.beginFill(0xFFFFFF, 0.5);
-block_wall.drawRect(0, 0, WIDTH, HEIGHT * 0.75);
+block_wall.drawRect(0, 0, WIDTH, HEIGHT * 0.55);
 block_wall.endFill();
 app.stage.addChild(block_wall);
 block_wall.zIndex = 50;
@@ -164,7 +165,10 @@ loader
 
 //加入場景
 function setup() {
-  germs_pop = [];
+  germs_no = 0;
+  germs_alive = []; //細菌活著
+  germs_pop = ['5ways',[],[],[],[],[]];
+  gameScene = new Container;
   bg_container = new Container;
   all_obj_container = new Container;
   show_console('health = 300');
@@ -174,7 +178,7 @@ function setup() {
   // action_gsap[i].pause();
   // action_gsap[i].play();
 
-  app.stage.addChild(all_obj_container);
+  gameScene.addChild(all_obj_container);
   //背景<
   bg_sprite.width = bg_sprite.width * (WIDTH / bg_sprite.width);
   bg_sprite.height = bg_sprite.height * (HEIGHT / bg_sprite.height);
@@ -182,6 +186,7 @@ function setup() {
   bg_sprite.x = 0;
   bg_sprite.y = 0;
   bg_container.addChild(bg_sprite);
+  app.stage.addChild(gameScene);
   app.stage.addChild(bg_container);
   bg_container.zIndex = -1;
   // show_console(bg_container.width);
@@ -199,9 +204,14 @@ function setup() {
     }
 
   }
+
+
+
+
   //執行遊戲
   state = play;
   app.ticker.add(delta => gameLoop(delta));
+  gameScene.visible = true;
 }
 
 let is_run = true;
@@ -238,11 +248,15 @@ function play(delta) {
   // if(out_contain( germs_container[2], {x: 0, y: 0, width: WIDTH, height: HEIGHT + 200}) != undefined == health > 0 ){
   //   show_console('health = ' + (health-=1));
   // }
-  if(player_action) {
+  if (player_action) {
     creatGerms();
   }
 
   removeGerms();
+
+  add_key_action();
+
+
   // germs_pop.forEach(function(_germs) {
   //
   // });
@@ -253,6 +267,25 @@ function play(delta) {
 
 }
 
+function add_key_action(){
+  for(let k=1 ;k< 6 ;k++) {
+    keys[k].press = function () {
+      for (let i = 0; i < germs_pop[k].length; i++) {
+        let aBox = germs_pop[k][i].getBounds();
+        let bBox = block_wall.getBounds();
+        let res = aBox.x + aBox.width > bBox.x &&
+          aBox.x < bBox.x + bBox.width &&
+          aBox.y + aBox.height > bBox.y &&
+          aBox.y < bBox.y + bBox.height;
+        if (!res) {
+          germs_pop[k][i].alpha = 0.2;
+          germs_alive[germs_no] = false;
+        }
+        console.log(res);
+      }
+    };
+  }
+}
 //動作
 function addInteraction(obj) {
   obj.interactive = true;
@@ -277,12 +310,18 @@ function rectsIntersect(a, b) {
     aBox.y < bBox.y + bBox.height;
 }
 
+let temp_r_i = 0;
 function creatGerms() {
   //set 敵人
   // show_console('creatGerms' + germs_pop.length);
-  //根据最大敌车数创建车子
-  for (let i = 0; i < numberOfgerms_pop - germs_pop.length; i++) {
+  // console.log('germs_pop.length' + germs_pop.length);
+  let all_pop = germs_pop[1].length + germs_pop[2].length + germs_pop[3].length + germs_pop[4].length +germs_pop[5].length ;
+  let get_rand = get5WayArr(level)[getRandomInt(0,4)];
+  // console.log(get_rand);
+  for (let i = 0; i < numberOfgerms_pop - all_pop; i++) {
 //細菌<
+    germs_no++;
+    // show_console( 'create_' + germs_no  );
     let _germs_container = [];
     let _germs = [
       'sprite'
@@ -292,47 +331,42 @@ function creatGerms() {
       , new Sprite(loader.resources["germs_4"].texture)
       , new Sprite(loader.resources["germs_5"].texture)
     ];
-    // for (let i = 1; i < 6; i++) {
-    let i = getRandomInt(1,5);
-      _germs_container[i] = new Container;
-      _germs[i].anchor.x = 0.5;
-      _germs[i].anchor.y = 0.5;
-      _germs[i].width = _germs[i].width * (WIDTH / bg_sprite.width) / 2;
-      _germs[i].height = _germs[i].height * (HEIGHT / bg_sprite.height) / 2;
-      // show_console('_germs[' + i + '].width-' + _germs[i].width);
-      _germs[i].x = 0;
-      _germs[i].y = 0;
-      _germs_container[i].addChild(_germs[i]);
-      all_obj_container.addChild(_germs_container[i]);
-      _germs_container[i].zIndex = i + 10;
-      _germs_container[i].x = (WIDTH - _germs_container[i].width) / 2 + (i * 10);
-      _germs_container[i].y = (HEIGHT - _germs_container[i].height) / 2 + (i * 10);
-      // show_console('germs -' + i);
-      //細菌加入動作
-      _germs_container[i].position.copyFrom(iso_path_array[i][0]);
-      _germs_container[i].pivot.set(0.5);
-      _germs_container[i].scale.set(germs_origin_fade_in);
-      action_gsap[i] = gsap.to(_germs_container[i], {
-        pixi: {scale: germs_origin_fade_out},
-        duration: germs_speed * getRandomInt(1, 5) + germs_speed_base ,
-        repeat: 1,
-        motionPath: {
-          path: iso_path_array[i]
-        },
-        ease: Power2.easeIn
-      });
-      //物件監聽動作
-      addInteraction(_germs_container[i]);
-      keys[i].press = function () {
-        if (!rectsIntersect(_germs_container[i], block_wall)) {
-          show_console('germs -' + i);
-        }
-      };
-      germs_pop.push(_germs_container[i]);
-      // show_console('germs_pop.push' + germs_pop.length)
-      // show_console('germs_pop[i].y - ' + _germs_container[i].height);
+    // let r_i = getRandomInt(1,5);
+    let r_i = get_rand[i];
+    console.log(r_i);
+    _germs_container[r_i] = new Container;
+    _germs[r_i].anchor.x = 0.5;
+    _germs[r_i].anchor.y = 0.5;
+    _germs[r_i].width = _germs[r_i].width * (WIDTH / bg_sprite.width) / 2;
+    _germs[r_i].height = _germs[r_i].height * (HEIGHT / bg_sprite.height) / 2;
+    // show_console('_germs[' + i + '].width-' + _germs[r_i].width);
+    _germs[r_i].x = 0;
+    _germs[r_i].y = 0;
+    _germs_container[r_i].addChild(_germs[r_i]);
+    _germs_container[r_i].zIndex = i + 10;
+    _germs_container[r_i].x = (WIDTH - _germs_container[r_i].width) / 2 + (i * 10);
+    _germs_container[r_i].y = (HEIGHT - _germs_container[r_i].height) / 2 + (i * 10);
+    // show_console('germs -' + i);
+    //細菌加入動作
+    _germs_container[r_i].position.copyFrom(iso_path_array[r_i][0]);
+    _germs_container[r_i].pivot.set(0.5);
+    _germs_container[r_i].scale.set(germs_origin_fade_in);
+    action_gsap[r_i] = gsap.to(_germs_container[r_i], {
+      pixi: {scale: germs_origin_fade_out},
+      duration: germs_speed * getRandomInt(1, 3) + germs_speed_base,
+      repeat: 1,
+      motionPath: {
+        path: iso_path_array[r_i]
+      },
+      ease: "slow(0.7, 0.7, false)"
+    });
+    _germs_container[r_i].alpha = 1;
+    console.log(_germs_container[r_i])
+    addInteraction(_germs_container[r_i]);
+     all_obj_container.addChild(_germs_container[r_i]);
+    germs_pop[r_i].push(_germs_container[r_i]);
+    all_pop = germs_pop[1].length + germs_pop[2].length + germs_pop[3].length + germs_pop[4].length +germs_pop[5].length ;
 
-    // }
   }
 
 
@@ -341,33 +375,32 @@ function creatGerms() {
 function removeGerms() {
   // show_console('removeGerms');
   // show_console('HEIGHT - ' + (HEIGHT + germs_origin_height));
-  for (let i = 0; i < germs_pop.length; i++) {
-    //
-    // show_console('germs_pop[i] - ' + germs_pop[i].height);
-    // show_console('germs_pop[i].y - ' + germs_pop[i].y);
-    // show_console('HEIGHT = ' + HEIGHT);
-
-    if (germs_pop[i].y > (HEIGHT) ) {
-      // show_console('health_width = ' + health_width);
-      // show_console('damage = ' + damage);
-      // show_console('360 - health = ' + (health_width_in -= (health_width * damage_ratio)));
-      // show_console('361 - health_width_in = ' + health_width_in);
-      health_width_in = health_width_in - damage;
-      if(health_width_in  > damage){
-        healthBar.outer.width = health_width_in;
-      }else{
-        healthBar.outer.width = 0;
-
+  // console.log(all_obj_container.children.length );
+  for (let i = 0; i <  germs_pop.length; i++) {
+    for(let j = 0 ; j < germs_pop[i].length; j++) {
+      if (germs_pop[i][j].y > (HEIGHT + 100)) {
+        if (germs_pop[i][j].alpha > 0.95) {
+          health_width_in = health_width_in - damage;
+          if (health_width_in > damage && countdown > 0) {
+            healthBar.outer.width = health_width_in;
+          } else {
+            healthBar.outer.width = 0;
+          }
+        }
+        //刪除陣列
+        germs_pop[i].splice(j, 1);
+        //刪除物件
+        if (all_obj_container.children.length > 0) {
+          all_obj_container.removeChildAt(j);
+        }
       }
-
-
-      //刪除陣列
-      germs_pop.splice(i, 1);
-      //刪除物件
-       // all_obj_container.removeChildAt(i);
     }
   }
+  if(healthBar.outer.width  <= 0){
+    gameScene.visible = false;
+  }
 }
+
 
 //按鍵方法
 function keyboard(keyCode) {
@@ -451,4 +484,25 @@ function hitTestRectangle(r1, r2) {
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+function wait(duration = 0) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, duration);
+  });
+}
+
+function get5WayArr(lv){
+    let arr = [];
+    if(lv == 3){
+      arr = [
+        [2,3,4,1,5],
+        [1,2,3,4,5],
+        [4,5,3,2,1],
+        [2,3,4,5,1],
+        [3,4,1,5,2]
+      ];
+    }
+  return arr;
 }
